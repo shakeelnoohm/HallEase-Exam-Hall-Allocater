02438db2c484ed1689239e8c2fa619a48a5e2aff
shakeelnoohm <faizinooh5555@gmail.com>
2025-10-15 23:57:43 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -17,5 +17,5 @@ app.use('/api/room', require('./routes/room'));
 app.use('/api/student', require('./routes/student'));
 app.use('/api/allotment', require('./routes/allotment'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
